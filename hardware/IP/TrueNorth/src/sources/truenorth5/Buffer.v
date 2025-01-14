705da65e21de49d68bdb230fd25ce6263ac378de
shunriokuhira <shunriaoping99@gmail.com>
2025-01-15 01:11:44 +0900

    edit Truenorth5 merge, buffer Its a shame

--- hardware/IP/TrueNorth/src/sources/truenorth5/Buffer.v
+++ hardware/IP/TrueNorth/src/sources/truenorth5/Buffer.v
@@ -36,6 +36,7 @@ module buffer#(
     reg [BUFFER_WIDTH:0] status_counter;
     reg [DATA_WIDTH-1:0] output_data;
     reg [DATA_WIDTH-1:0] din_before;
+    reg [DATA_WIDTH-1:0] din_before2;
     wire [DATA_WIDTH-1:0] buff_0, buff_1, buff_2, buff_3;
     assign buff_0 = data[0];
     assign buff_1 = data[1];
@@ -47,13 +48,15 @@ module buffer#(
     assign dout = output_data;
 
     wire write_twice;//バッファが同じパケットを二回連続して書き込もうとしたことを検知
-    assign write_twice = (din == din_before) ? 1 : 0;
+    assign write_twice = (din == din_before2) ? 1 : 0;
     always @(posedge clk) begin
         if(rst)begin
             din_before <= 0;
+            din_before2 <= 0;
         end
         else if(din_valid)begin
             din_before <= din;
+            din_before2 <= din_before;
         end
     end
 
@@ -77,7 +80,7 @@ module buffer#(
             output_data <= 0;
         end
         else begin
-            if (!full && din_valid) begin
+            if (!full && din_valid && !write_twice) begin
             //if (!full && din_valid) begin
                 data[write_pointer] = din;
                 write_pointer = write_pointer + 1;
@@ -89,9 +92,9 @@ module buffer#(
                 read_pointer = read_pointer + 1;
                 status_counter = status_counter - 1;
             end
-            // if(write_twice)begin
-            //     output_data <= 0;
-            // end
+            if(write_twice || din_zero)begin
+                output_data <= 0;
+            end
         end
     end
 
--- hardware/IP/TrueNorth/src/sources/truenorth5/Merge2.v
+++ hardware/IP/TrueNorth/src/sources/truenorth5/Merge2.v
@@ -16,24 +16,137 @@ module Merge2#(
 );
 
 
-reg [DATA_WIDTH-1:0] din_from_local_before;
-reg [DATA_WIDTH-1:0] din_from_routing_before;
-wire read_twice_local, read_twice_routing;//バッファが同じパケットを二回連続して書き込もうとしたことを検知
-assign read_twice_local = (din_from_local_before == din_from_local) ? 1 : 0;
-assign read_twice_routing = (din_from_routing_before == din_from_routing) ? 1 : 0;
-always @(negedge clk) begin
-    if(rst)begin
-        din_from_local_before <= 0;
-        din_from_routing_before <= 0;
-    end
-    else if(men_from_local)begin
-        din_from_local_before <= din_from_local;
-    end
-    else if(men_from_routing)begin
-        din_from_routing_before <= din_from_routing;
-    end
-end
 
+
+//merge_v3
+// reg[3:0] state = 1;
+// localparam wait_routing = 0, wait_local = 1;
+// always @(negedge clk) begin
+//     if(rst)begin
+//         merge_out <= 0;
+//         merge_wen <= 0;
+//         wait_to_local <= 0;
+//         wait_to_routing <= 0;
+//         state <= wait_local;
+//     end
+//     else if(!full)begin
+//         if(men_from_routing & !men_from_local)begin//routing側だけからリクエストがきたとき
+//             merge_out <= din_from_routing;
+//             merge_wen <= 1;
+//             wait_to_routing <= 0;
+//             wait_to_local <= 0;
+//             state <= wait_routing;
+//         end
+//         else if(!men_from_routing & men_from_local)begin//local側だけからリクエストきたとき
+//             merge_out <= din_from_local;
+//             merge_wen <= 1;
+//             wait_to_routing <= 0;
+//             wait_to_local <= 0;
+//             state <= wait_local;
+//         end
+//         else if(men_from_routing & men_from_local)begin//競合したとき
+//             case(state)
+//                 wait_routing:begin
+//                     wait_to_routing <= 1;
+//                     wait_to_local <= 0;
+//                     merge_out <= din_from_local;
+//                     merge_wen <= 1;
+//                     state <= wait_local;
+//                 end
+
+//                 wait_local:begin
+//                     wait_to_routing <= 0;
+//                     wait_to_local <= 1;
+//                     merge_out <= din_from_routing;
+//                     merge_out <= 1;
+//                     state <= wait_routing;
+//                 end
+//             endcase
+//         end
+//         else begin
+//             wait_to_routing <= 0;
+//             wait_to_local <= 0;
+//             merge_out <= 0;
+//             merge_out <= 0;
+//             state <= wait_local;
+//         end
+        
+//     end
+// end
+
+
+
+
+//merge_v2
+// reg [DATA_WIDTH-1:0] din_from_local_before;
+// reg [DATA_WIDTH-1:0] din_from_routing_before;
+// wire read_twice_local, read_twice_routing;//バッファが同じパケットを二回連続して書き込もうとしたことを検知
+// assign read_twice_local = (din_from_local_before == din_from_local) ? 1 : 0;
+// assign read_twice_routing = (din_from_routing_before == din_from_routing) ? 1 : 0;
+// always @(negedge clk) begin
+//     if(rst)begin
+//         din_from_local_before <= 0;
+//         din_from_routing_before <= 0;
+//     end
+//     else if(men_from_local)begin
+//         din_from_local_before <= din_from_local;
+//     end
+//     else if(men_from_routing)begin
+//         din_from_routing_before <= din_from_routing;
+//     end
+// end
+// always @(negedge clk) begin
+//     if(rst)begin
+//         merge_out <= 0;
+//         merge_wen <= 0;
+//         wait_to_local <= 0;
+//         wait_to_routing <= 0;
+//     end
+//     else if(!full)begin
+//         if(men_from_routing)begin//このフラグが立ってるあいだはパケット取り込める
+//             if(men_from_local)begin//外部パケット取り込み中にローカルパケットの取り込みのリクエストがきたとき
+//                 wait_to_local <= 1;
+//             end
+//             if(read_twice_routing)begin
+//                 merge_out <= 0;
+//                 merge_wen <= 0;
+//                 wait_to_routing <= 0;
+//             end
+//             else begin
+//                 merge_out <= din_from_routing;//外部パケット取り込み
+//                 merge_wen <= 1;//バッファへの書き込み許可
+//                 wait_to_routing <= 0;
+//             end
+//         end
+//         else if(men_from_local)begin
+//             if(men_from_routing)begin
+//                 wait_to_routing <= 1;
+//             end
+//             if(read_twice_local)begin
+//                 merge_out <= 0;
+//                 merge_wen <= 0;
+//                 wait_to_local <= 0;
+//             end
+//             else begin
+//                 merge_out <= din_from_local;
+//                 merge_wen <= 1;
+//                 wait_to_local <= 0;
+//             end
+//         end
+//         else begin
+//             merge_wen <= 0;
+//             //merge_out <= 0;
+//             wait_to_local <= 0;
+//             wait_to_routing <= 0;
+//         end
+//     end
+// end
+
+
+
+
+
+//merge_v1
 always @(negedge clk) begin
     if(rst)begin
         merge_out <= 0;
@@ -46,17 +159,6 @@ always @(negedge clk) begin
             if(men_from_local)begin//外部パケット取り込み中にローカルパケットの取り込みのリクエストがきたとき
                 wait_to_local <= 1;
             end
-
-            // if(read_twice_routing)begin
-            //     merge_out <= 0;
-            //     merge_wen <= 0;
-            //     wait_to_routing <= 0;
-            // end
-            // else begin
-            //     merge_out <= din_from_routing;//外部パケット取り込み
-            //     merge_wen <= 1;//バッファへの書き込み許可
-            //     wait_to_routing <= 0;
-            // end
             merge_out <= din_from_routing;//外部パケット取り込み
             merge_wen <= 1;//バッファへの書き込み許可
             wait_to_routing <= 0;
@@ -65,24 +167,14 @@ always @(negedge clk) begin
             if(men_from_routing)begin
                 wait_to_routing <= 1;
             end
-            // if(read_twice_local)begin
-            //     merge_out <= 0;
-            //     merge_wen <= 0;
-            //     wait_to_local <= 0;
-            // end
-            // else begin
-            //     merge_out <= din_from_local;
-            //     merge_wen <= 1;
-            //     wait_to_local <= 0;
-            // end
-                merge_out <= din_from_local;
-                merge_wen <= 1;
-                wait_to_local <= 0;
+            merge_out <= din_from_local;
+            merge_wen <= 1;
+            wait_to_local <= 0;
 
         end
         else begin
             merge_wen <= 0;
-            //merge_out <= 0;
+            merge_out <= 0;
             wait_to_local <= 0;
             wait_to_routing <= 0;
         end
